5858032c12109b74e2d3950e4f8e2af14f0dd039
Ashley Clarke <me@ashleyclarke.me>
2016-01-29 14:31:02 +0000

    add some composer tests

--- tests/ComposerTest.php
+++ tests/ComposerTest.php
@@ -77,4 +77,47 @@ class ComposerTest extends PHPUnit_Framework_TestCase
 
         new Composer($file);
     }
+
+    /**
+     * @test
+     */
+    public function it_gets_a_classes_namespace()
+    {
+        $namespace = $this->composer->getClassNamespace('Item');
+        $this->assertEquals('Example\Item', $namespace);
+
+        // it shouldn't care if the root ns is provided.
+        $namespace = $this->composer->getClassNamespace('Example\Item');
+        $this->assertEquals('Example\Item', $namespace);
+    }
+
+    /**
+     * @test
+     */
+    public function it_gets_a_classes_path()
+    {
+        $path = $this->composer->getClassPath('Item');
+        $this->assertEquals('src/Item.php', $path);
+
+        $path = $this->composer->getClassPath('Example\Item');
+        $this->assertEquals('src/Item.php', $path);
+
+        $path = $this->composer->getClassPath('Some\Item');
+        $this->assertEquals('src/Some/Item.php', $path);
+    }
+
+    /**
+     * @test
+     */
+    public function it_gets_the_test_path_for_a_class()
+    {
+        $path = $this->composer->getTestPath('Item');
+        $this->assertEquals('tests/ItemTest.php', $path);
+
+        $path = $this->composer->getTestPath('Example\Item');
+        $this->assertEquals('tests/ItemTest.php', $path);
+
+        $path = $this->composer->getTestPath('Some\Item');
+        $this->assertEquals('tests/SomeItemTest.php', $path);
+    }
 }
