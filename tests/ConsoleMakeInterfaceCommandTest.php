546b6c2ed19a4e10e412effaca4a28830019a4ba
Ashley Clarke <me@ashleyclarke.me>
2016-02-01 00:36:06 +0000

    test make:interface

--- tests/ConsoleMakeInterfaceCommandTest.php
+++ tests/ConsoleMakeInterfaceCommandTest.php
@@ -2,6 +2,7 @@
 
 use Factory\Application;
 use Factory\Console\Make\InterfaceCommand;
+use Symfony\Component\Console\Tester\CommandTester;
 
 class ConsoleMakeInterfaceCommandTest extends TestCase
 {
@@ -19,8 +20,57 @@ class ConsoleMakeInterfaceCommandTest extends TestCase
     /**
      * @test
      */
-    public function it_works()
+    public function it_makes_a_basic_interface()
     {
-        $this->assertTrue(true);
+        $command = $this->app->find('make:interface');
+        $commandTester = new CommandTester($command);
+
+        $commandTester->execute([
+            'name' => 'Tests/BasicInterface'
+        ]);
+
+        $this->assertFileExists(__DIR__ . '/../src/Tests/BasicInterface.php');
+        $this->assertFileEquals(__DIR__ . '/stubs/BasicInterface.txt', __DIR__ . '/../src/Tests/BasicInterface.php');
+    }
+
+    /**
+     * @test
+     * @expectedException \League\Flysystem\FileExistsException
+     */
+    public function it_wont_overwrite_a_file_by_default()
+    {
+        mkdir(__DIR__ . '/../src/Tests');
+        touch(__DIR__ . '/../src/Tests/BasicInterface.php');
+        $this->assertFileExists(__DIR__ . '/../src/Tests/BasicInterface.php');
+
+        $command = $this->app->find('make:interface');
+        $commandTester = new CommandTester($command);
+
+        $commandTester->execute([
+            'name' => 'Tests/BasicInterface'
+        ]);
+
+        $this->assertEmpty(file_get_contents(__DIR__ . '/../src/Tests/BasicInterface.php'));
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_be_forced_to_overwrite_a_file()
+    {
+        mkdir(__DIR__ . '/../src/Tests');
+        touch(__DIR__ . '/../src/Tests/BasicInterface.php');
+        $this->assertFileExists(__DIR__ . '/../src/Tests/BasicInterface.php');
+
+        $command = $this->app->find('make:interface');
+        $commandTester = new CommandTester($command);
+
+        $commandTester->execute([
+            'name' => 'Tests/BasicInterface',
+            '--force' => true,
+        ]);
+
+        $this->assertFileExists(__DIR__ . '/../src/Tests/BasicInterface.php');
+        $this->assertFileEquals(__DIR__ . '/stubs/BasicInterface.txt', __DIR__ . '/../src/Tests/BasicInterface.php');
     }
 }
